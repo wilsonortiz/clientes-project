e47ebe5e725a8099bf17cb8445a19bb4854eb117
Wilson Ortiz Troncoso <wilson@localhost.localdomain>
2019-05-21 13:52:00 -0400

    Se agrega gralphql y el primer schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+//graphql
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: false,
+      playground: true,
+      typePaths: ['./**/*.graphql']
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
